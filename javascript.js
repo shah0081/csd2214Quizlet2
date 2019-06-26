f73d9e16fdbd25a8c77693c63ad5f3a9602354bd
Manmohitpreet Singh <755086@student.tor.loc>
2019-06-26 15:08:49 -0400

    3pm

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function waitingfn(timeinms){
+const futuretime = date.now() +timeinms;
+while (futuretime > date.now()){
+
+}
+
+
+}
+debugger;
+    
+waitingfn(3000)
+console.log("function called has just ended");
